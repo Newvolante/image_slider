f0eec8da52bafa978545721c24e630f17ea20609
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-05-21 12:25:56 +0200

    added an id attribute to a elements and a function in script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 $(function() {
-    
+    $hrefImg1 = $('a#red').attr('href');
+    $hrefImg2 = $('a#green').attr('href');
+    $hrefImg3 = $('a#blue').attr('href');
+
+    $('.btn').on('click', function() {
+        
+    });
 });

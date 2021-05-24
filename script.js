1cabbd2ceb111afe3a44e29d52aca0bdeed82eea
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-05-24 09:14:21 +0200

    added code to highlight active button - added css for active button

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ $(function() {
         $hrefImg3 = $('a#blue').attr('href')
     ];
 
-    let index = 0;      // control value to make the sliding cyclic
+    // array that holds buttons
+    let buttons = [ $('#red'), $('#green'), $('#blue')];
+    
+    // control value to make the sliding cyclic
+    let index = 0;      
 
     // the interval goes on infinitely and calls automaticSliding()
     function timer() {
@@ -19,13 +23,16 @@ $(function() {
             automaticSliding(index);               // uncomment to make timer start again
         }, 1000);
     }
-
+    
     // changes the image shown
     function automaticSliding() {
+        $('.active').removeClass('active');
+        console.log('removing...');
         if (index < 3) {
             console.log('automaticSliding called...');
             $tempSlideImg = '<img id="tempSlider" src="./images/' + images[index] + '.png" />';
             $('div#slider').append($tempSlideImg);
+            buttons[index].addClass('active');
             index += 1;
         } else {
             index = 0;
